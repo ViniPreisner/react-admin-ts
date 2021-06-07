571966a83cbff92f6eb5e19d595aeb9e75816037
Vinicius do Nascimento <vinipreisner@gmail.com>
2021-06-07 20:57:05 -0300

    wip(*): estudando outros hooks

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, {
+    useState,
+    useEffect,
+    useMemo,
+    useRef,
+    useContext, 
+    useReducer
+}
+from 'react'
 
-type Props = {}
 
-const Produtos = (props: Props) => {
+interface User {
+    name: string;
+    login: string;
+    avatar_url: string;
+}
+
+const Produtos: React.FC = () => {
+
+    const [user, setUser] = useState<User>()
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    interface Cart {
+        products: string[];
+        shipping_value?: number;
+    }
+
+    type CartActionType = {
+        type: 'ADD_PRODUCT' | 'REMOVE_PRODUCT'
+    }
+
+    const cart = useReducer(
+        (state: Cart, action: CartActionType) => {
+            switch(action.type) {
+                case 'ADD_PRODUCT':
+                    return {
+                        ...state,
+                        products: [...state.products, 'Produto novo']
+                    }
+                default:
+                    return state
+            }
+        },
+        { // valor inicial do objeto
+            products: ['asd'],
+            shipping_value: 0
+        }
+    )
+
+        async function loadData() {
+            const response = await fetch('https://api.github.com/users/vinipreisner')
+            const data = await response.json()
+
+            setUser(data)
+        }
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
+    inputRef.current?.focus()
+
     return (
-        <div>Produtos</div>
+        <div>
+            <h1>Produtos</h1>
+            <form>
+                <input ref={inputRef}>
+                </input>
+            </form>
+        </div>
     )
 }
 
