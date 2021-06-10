0dd867a1ccece3d3d0719647ca6157baed17624d
Vinicius do Nascimento <vinipreisner@gmail.com>
2021-06-09 21:02:52 -0300

    feat(*): users init

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,11 +1,50 @@
-import React, { useImperativeHandle } from 'react'
+import React, { Component } from 'react'
 
 type Props = {}
+type State = {
+    count: number;
+    hide: boolean
+}
 
-const Users = (props: Props) => {
-    return (
-        <div>Users</div>
-    )
+class Users extends Component<Props, State> {
+
+    state: State = {
+        count: 0,
+        hide: false
+    }
+
+    increment = (amt: number) => {
+        this.setState((state) => ({
+            count: state.count + amt,
+            //hide: state.hide
+        }))
+    }
+
+    toggleVisibility = () => {
+        this.setState((state) => ({
+            hide: !state.hide,
+            //count: state.count
+        }))
+    }
+
+    render() {
+        return (
+            <>
+            <div>
+                {!this.state.hide &&
+                <a href="#" onClick={() => this.increment(1)}>Users count: {this.state.count}</a>
+                }
+            </div>
+            <div>
+                
+                <a href="#" onClick={() => this.toggleVisibility()}>
+                {!this.state.hide ? 'Ocultar' : 'Mostrar' }
+                </a>
+
+            </div>
+            </>
+        )
+    }
 }
 
 export default Users
